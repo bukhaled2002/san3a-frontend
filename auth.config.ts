222e5bbbc5c5299b6f38323c9f0e35b6a1945a97
bukhaled2002 <Bakhaled.310@gmail.com>
2026-03-15 03:50:08 +0200

    fix: resolve __dirname error and MissingSecret in production

--- auth.config.ts
+++ auth.config.ts
@@ -2,6 +2,7 @@ import type { NextAuthConfig } from "next-auth";
 import { protectedRoutes } from "./routes";
 
 export const authConfig = {
+  secret: process.env.AUTH_SECRET,
   pages: {
     signIn: "/",
     signOut: "/",
