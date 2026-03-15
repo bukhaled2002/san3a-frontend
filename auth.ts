cb507ef8b9791f6aa814a95f4227c2d54724074a
bukhaled2002 <Bakhaled.310@gmail.com>
2026-03-15 05:07:22 +0200

    ss

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthConfig } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { authConfig } from "./auth.config";
+import { adminAPI, parentAPI, studentAPI, teacherAPI } from "./services/axios";
+import { protectedRoutes } from "./routes";
 
 const credentialsConfig = CredentialsProvider({
   credentials: {
@@ -9,44 +10,64 @@ const credentialsConfig = CredentialsProvider({
     role: { label: "Role", type: "text" },
   },
   async authorize(credentials) {
-    const baseURL = process.env.NEXT_PUBLIC_SERVER_ENDPOINT;
-    const endpoint =
+    console.log(credentials);
+    const api =
       credentials?.role === "student"
-        ? "/student/login"
+        ? studentAPI
         : credentials?.role === "parent"
-        ? "/parent/login"
-        : credentials?.role === "teacher"
-        ? "/teacher/login"
-        : "/admin/login";
-
+          ? parentAPI
+          : credentials?.role === "teacher"
+            ? teacherAPI
+            : adminAPI;
     try {
-      const response = await fetch(baseURL + endpoint, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: credentials?.username,
-          password: credentials?.password,
-        }),
+      const response = await api.post("/login", {
+        username: credentials?.username,
+        password: credentials?.password,
       });
-
-      if (!response.ok) {
+      if (response.status === 200 || response.status === 201) {
+        return { ...response.data, role: credentials?.role };
+      } else {
         return null;
       }
-
-      const data = await response.json();
-      return { ...data, role: credentials?.role };
     } catch (error: any) {
-      console.error("Auth error:", error);
+      console.log(error.response.data);
       return null;
     }
   },
 });
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  ...authConfig,
+const config = {
   providers: [credentialsConfig],
-  secret: process.env.AUTH_SECRET || "Yo/0duPLAErkzTcBlgWGWR4eaVyivqU6a+M/ot0fo9c=",
-});
+  callbacks: {
+    async jwt({ token, user, trigger, session }) {
+      if (user) token.role = user.role;
+      if (trigger === "update") {
+        return { ...token, ...session };
+      }
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      session.user = token as any;
+      session.user.role = token.role;
+      return session;
+    },
+    authorized({ request, auth }) {
+      const protectedRoute = protectedRoutes.find((route) => {
+        return request.nextUrl.pathname.includes(route.path);
+      });
+      if (protectedRoute && !auth) {
+        return false;
+      }
+      return true;
+    },
+  },
+  pages: {
+    signIn: "/",
+    signOut: "/",
+  },
+  secret: "Yo/0duPLAErkzTcBlgWGWR4eaVyivqU6a+M/ot0fo9c=",
+} satisfies NextAuthConfig;
 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  ...config,
+});
